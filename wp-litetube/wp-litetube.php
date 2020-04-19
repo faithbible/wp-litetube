e02eddcbc22396dc4a79f778297e7c103d67174c
faithbible <mail@faith-bible.net>
2020-04-19 15:02:48 -0700

    Update wp-litetube.php
    add inline calls to js & cs

--- wp-litetube/wp-litetube.php
+++ wp-litetube/wp-litetube.php
@@ -9,15 +9,34 @@
    License: GPLv3
   */
 
-# add to stylesheet, add script, use element with shortcode
+# add conditional check for css before calling script in footer
 
+function test_if_it_exists() {
+$handle = 'fluidVids.js';
+    $list = 'enqueued';
+      if ( wp_script_is( $handle, $list ) ) {
+        return;
+      } else {
+        wp_register_script( 'fluidVids.js', plugin_dir_url(__FILE__).'js/fluidvids.min.js');
+        wp_enqueue_script( 'fluidVids.js' );
+      }
+}
+
+function LTscript(){
+  ?>
+  <script src="' .  plugin_dir_url( __FILE__ ) . 'lite-youtube/src/lite-yt-embed.js"></script>
+  <?php
+}
+# add_action( 'wp_footer', 'LTscript' );
+		
+# shortcode usage = [liteube id="youtube video id" img="url of video preview"]
 
 function LiteYouTube( $atts ) {
     $lt = shortcode_atts(array(
        'id' => 'MFHVBUNGXzI',
        'img' => 'https://placeimg.com/1024/576/nature'
     	), $atts);
-    return '<lite-youtube videoid="' . $a['id'] . '" style="background-image: url(' . $a['img'] . ');"><div class="lty-playbtn"></div></lite-youtube>';
+   return '<script src="' .  plugin_dir_url( __FILE__ ) . 'lite-youtube/src/lite-yt-embed.js"></script> \r\n <link rel="stylesheet" href="' .  plugin_dir_url( __FILE__ ) . 'lite-youtube/src/lite-yt-embed.css" /> \r\n <lite-youtube videoid="' . $a['id'] . '" style="background-image: url(' . $a['img'] . ');"><div class="lty-playbtn"></div></lite-youtube>';
     }
 add_shortcode('litetube', 'LiteYouTube');
 
